411e3f81af832a73edac68e6ab1238a13ad4fdf3
Cikgu Faiz <faiz.bek3991@gmail.com>
2026-03-12 12:08:02 +0800

    Initial commit: Add jQuery text change exercise

---
+++ script.js
@@ -0,0 +1,7 @@
+ // Your jQuery code will go here
+        $(document).ready(function() {
+            // Exercise 1: Change text on button click
+            $("#change-text").click(function() {
+                $("#text-to-change").text("This text has been changed with jQuery!");
+            });
+        });
