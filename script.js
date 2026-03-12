9535557e083fb83d2d90589eded6c91ca4ad6139
Cikgu Faiz <faiz.bek3991@gmail.com>
2026-03-12 12:23:18 +0800

    jquery js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
- // Your jQuery code will go here
-        $(document).ready(function() {
-            // Exercise 1: Change text on button click
-            $("#change-text").click(function() {
-                $("#text-to-change").text("This text has been changed with jQuery!");
-            });
-        });
-=======
 $(document).ready(function() {
     // Exercise 1: Change text on button click
     $("#change-text").click(function() {
         $("#text-to-change").text("This text has been changed with jQuery!");
     });
 });
->>>>>>> 39ec6ede11025fb4819a2d6ddb3c58b5207a203b
